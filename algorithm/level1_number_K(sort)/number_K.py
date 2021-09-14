26907dc1505ac0b1674eb574531a2c3be0916fce
kang <micro.kante@gmail.com>
2021-09-14 14:31:57 +0900

    K번째 수 - 프로그래머스 정렬 알고리즘, 슬라이승

--- algorithm/level1_number_K(sort)/number_K.py
+++ algorithm/level1_number_K(sort)/number_K.py
@@ -68,5 +68,8 @@ def solution(array, commands):
 
 print(solution(array,commands))
 
+####
 def solution(array, commands):
     return list(map(lambda x:sorted(array[x[0]-1:x[1]])[x[2]-1], commands))
+
+print(solution(array,commands))
